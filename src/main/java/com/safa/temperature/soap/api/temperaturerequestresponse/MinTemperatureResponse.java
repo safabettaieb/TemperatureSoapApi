480cdf49d7981849403b2774f356b83362fac788
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-26 20:43:03 +0100

    create xsd file and generate requests and responses classes

---
+++ src/main/java/com/safa/temperature/soap/api/temperaturerequestresponse/MinTemperatureResponse.java
@@ -0,0 +1,61 @@
+//
+// Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.7 
+// Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
+// Généré le : 2020.02.26 à 08:29:59 PM WAT 
+//
+
+
+package com.safa.temperature.soap.api.temperaturerequestresponse;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Classe Java pour anonymous complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="resTemp" type="{http://www.w3.org/2001/XMLSchema}int"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "resTemp"
+})
+@XmlRootElement(name = "MinTemperatureResponse")
+public class MinTemperatureResponse {
+
+    protected int resTemp;
+
+    /**
+     * Obtient la valeur de la propriété resTemp.
+     * 
+     */
+    public int getResTemp() {
+        return resTemp;
+    }
+
+    /**
+     * Définit la valeur de la propriété resTemp.
+     * 
+     */
+    public void setResTemp(int value) {
+        this.resTemp = value;
+    }
+
+}
