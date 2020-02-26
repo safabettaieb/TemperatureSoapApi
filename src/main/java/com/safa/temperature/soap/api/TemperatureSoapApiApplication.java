8df9c63de8213562e9db1d228ded041b45a9ec8d
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-26 18:45:06 +0100

    first commit

---
+++ src/main/java/com/safa/temperature/soap/api/TemperatureSoapApiApplication.java
@@ -0,0 +1,13 @@
+package com.safa.temperature.soap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureSoapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemperatureSoapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safa/temperature/soap/api/TemperatureSoapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safa.temperature.soap.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TemperatureSoapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
