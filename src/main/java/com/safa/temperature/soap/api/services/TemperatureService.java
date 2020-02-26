84c8632bafb6f3ac5d7479c0f16010cb37e1b277
Safa Bettaieb <safa.bettaieb1@gmail.com>
2020-02-26 21:34:23 +0100

    create TemperatureService and add min method

---
+++ src/main/java/com/safa/temperature/soap/api/services/TemperatureService.java
@@ -0,0 +1,24 @@
+package com.safa.temperature.soap.api.services;
+import java.util.Comparator;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.safa.temperature.soap.api.temperaturerequestresponse.MinTemperatureRequest;
+import com.safa.temperature.soap.api.temperaturerequestresponse.MinTemperatureResponse;
+
+
+@Service
+public class TemperatureService {
+	
+	public MinTemperatureResponse min(MinTemperatureRequest request) {
+
+		MinTemperatureResponse response = new MinTemperatureResponse();
+		List<Integer> liste= request.getTemperatures();
+		liste.sort(Comparator.naturalOrder());
+		response.setResTemp(liste.get(0));
+		return response;
+
+	}
+
+}
